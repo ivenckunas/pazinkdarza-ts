dbf79121a453e1ace913e693de624d1464388721
ivenckunas <ignas.venckunas@gmail.com>
2023-08-02 13:20:45 +0300

    prev next back buttons and functionality

--- src/Components/Details/Details.tsx
+++ src/Components/Details/Details.tsx
@@ -1,12 +1,36 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import './details.scss';
 import MainContext from '../../Context/MainContext';
 import dataArr from '../../data/dataArr';
 
 function Details() {
-	const {openedVeg} = useContext(MainContext);
-	const currentVeg = dataArr[openedVeg];
+	const {openedVeg, setOpenedVeg} = useContext(MainContext);
+
+	const [currentVeg, setCurrentVeg] = useState(dataArr[openedVeg]);
+	const [nextVeg, setNextVeg] = useState(null);
+	const [prevVeg, setPrevVeg] = useState(null);
+
+	useEffect(() => {
+		setCurrentVeg(dataArr[openedVeg]);
+		setNextVeg(dataArr[openedVeg + 1]);
+		setPrevVeg(dataArr[openedVeg - 1]);
+	}, [openedVeg]);
+
+	const handleNext = () => {
+		setOpenedVeg(openedVeg + 1);
+	};
+
+	const handlePrev = () => {
+		setOpenedVeg(openedVeg - 1);
+	};
+
+	const backToHomePage = () => {
+		setOpenedVeg(null);
+	};
+
 	const images = currentVeg.images;
+	const isFirstItem = openedVeg === 0;
+	const isLastItem = openedVeg === dataArr.length - 1;
 	return (
 		<div className='details-container'>
 			<div className='images-container'>
@@ -22,6 +46,11 @@ function Details() {
 				<h1>{currentVeg.name}</h1>
 				<h2>{currentVeg.type}</h2>
 				<p>{currentVeg.desc}</p>
+			</div>
+			<div className='buttons'>
+				{isFirstItem ? <button disabled>Atgal</button> : <button onClick={() => handlePrev()}>Atgal</button>}
+				{isLastItem ? <button disabled>Pirmyn</button> : <button onClick={() => handleNext()}>Pirmyn</button>}
+				<button onClick={() => backToHomePage()}>Grizti</button>
 			</div>
 		</div>
 	);
