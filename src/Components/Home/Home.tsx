95eb698a69fc24e0d1ac3dba41da25e73855322c
ivenckunas <ignas.venckunas@gmail.com>
2023-08-03 17:47:31 +0300

    bg changed, grid to flex

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,10 +2,14 @@ import './home.scss';
 import VegGrid from '../VegGrid/VegGrid';
 import SearchBar from '../SearchBar/SearchBar';
 import Footer from '../Footer/Footer';
+import {useContext} from 'react';
+import MainContext from '../../Context/MainContext';
 
 function Home() {
+	const {openedVeg} = useContext(MainContext);
+
 	return (
-		<div className='container'>
+		<div className={openedVeg === null ? 'container container-bg' : 'container'}>
 			<SearchBar />
 			<VegGrid />
 			<Footer />
