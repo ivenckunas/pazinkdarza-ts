52f3b7d7dd7cd00fb031046373ca1ad0fbeb1c2a
ivenckunas <ignas.venckunas@gmail.com>
2023-08-22 10:41:07 +0300

    mobile bg changed

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -8,18 +8,32 @@ function Footer() {
 				<p>
 					Visa info iš: G.Visockienė "Jaunajam daržininkui" ir "Sodininko ir daržininko mėgejo žinynas". <br /> Abi knygos man kainavo 3.6eur.
 				</p>
-				<a
-					href='https://www.linkedin.com/in/ignas-venckunas/'
-					target='_blank'
-				>
-					<BsLinkedin />
-				</a>
-				<a
-					href='https://github.com/ivenckunas'
-					target='_blank'
-				>
-					<BsGithub />
-				</a>
+				<div className='credits'>
+					<div className='dev'>
+						<p>Radai klaidą, nori kažką pasiūlyti ar papildyti? Rašyk - Ignas V.</p>
+						<a
+							href='https://www.linkedin.com/in/ignas-venckunas/'
+							target='_blank'
+						>
+							<BsLinkedin />
+						</a>
+						<a
+							href='https://github.com/ivenckunas'
+							target='_blank'
+						>
+							<BsGithub />
+						</a>
+					</div>
+					<div className='art'>
+						<p>Daržovių pixel art - Anton P.</p>
+						<a
+							href='https://www.linkedin.com/in/ignas-venckunas/'
+							target='_blank'
+						>
+							<BsLinkedin />
+						</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
