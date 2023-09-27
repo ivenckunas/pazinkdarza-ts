56aa7510daf958078ba0e9f4a1b90abb105ad9a6
ivenckunas <ignas.venckunas@gmail.com>
2023-09-27 12:17:12 +0300

    pataisytas agurku aprasymas

---
+++ src/data/blogPosts.tsx
@@ -0,0 +1,17 @@
+export type blogPost = {
+	title: string;
+	date: number;
+	content: string;
+	author: string;
+};
+
+const blogPostsArr: blogPost[] = [
+	{
+		title: 'aaa',
+		date: 2023,
+		content: 'aaaaaaaa',
+		author: 'aaaaaaaaaaaaa',
+	},
+];
+
+export default blogPostsArr;
