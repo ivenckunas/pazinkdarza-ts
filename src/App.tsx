deab30d1b242bd001e170af831aedb402692e31e
ivenckunas <ignas.venckunas@gmail.com>
2023-10-03 14:24:38 +0300

    unused items commented for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Home from './Components/Home/Home';
 import MainContext from './Context/MainContext';
 import {Veg} from './Context/MainContext';
 import Details from './Components/Details/Details';
-import Favorites from './Components/Details/Favorites';
+// import Favorites from './Components/Details/Favorites';
 
 function App() {
 	const [openedVeg, setOpenedVeg] = useState<null | number>(null);
@@ -23,10 +23,10 @@ function App() {
 						element={<Details />}
 						path={`/darzoves/:id`}
 					/>
-					<Route
+					{/* <Route
 						element={<Favorites />}
 						path={`/pazymetos`}
-					/>
+					/> */}
 				</Routes>
 			</BrowserRouter>
 		</MainContext.Provider>
